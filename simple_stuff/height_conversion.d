5da37a099415892b27da51a2dc943ea5e4371cdc
Evan Duncan <evan.duncan@target.com>
2015-02-10 11:47:20 -0600

    chapter 1 section 2

--- simple_stuff/height_conversion.d
+++ simple_stuff/height_conversion.d
@@ -5,12 +5,12 @@
 import std.stdio;
 
 void main() {
-	immutable inchesPerFoot = 12;
-	immutable cmPerInch = 2.54;
+    immutable inchesPerFoot = 12;
+    immutable cmPerInch = 2.54;
 
-	foreach (feet; 5 .. 7) {
-		foreach (inches; 0 .. inchesPerFoot) {
-			writeln("feet: ", feet, "'", inches, "\"\tcm: ", (feet * inchesPerFoot + inches) * cmPerInch);
-		}
-	}
+    foreach (feet; 5 .. 7) {
+        foreach (inches; 0 .. inchesPerFoot) {
+            writefln("feet: %s'%s\"\tcm: %s", feet, inches, (feet * inchesPerFoot + inches) * cmPerInch);
+        }
+    }
 }
