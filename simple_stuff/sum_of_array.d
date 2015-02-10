fff697a34f2a8a77aab3c3cf04fe24310c71f08c
Evan Duncan <evan.duncan@target.com>
2015-02-10 10:09:51 -0600

    sum an array

---
+++ simple_stuff/sum_of_array.d
@@ -0,0 +1,29 @@
+import std.stdio;
+import std.array;
+
+void main()
+{
+    int[] test_array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    int[] empty_array = [];
+    writeln(sum_array(test_array));
+    writeln(sum_array(empty_array));
+
+    writeln(sum_array_foreach(test_array));
+    writeln(sum_array_foreach(empty_array));
+}
+
+int sum_array(int[] input_array)
+{
+    int sum;
+    for (size_t i = 0; i < input_array.length; i++)
+        sum += input_array[i];
+    return sum;
+}
+
+int sum_array_foreach(int[] input_array)
+{
+    int sum;
+    foreach (size_t index, int value; input_array)
+        sum += input_array[index];
+    return sum;
+}
