23aaa92ab55f24257eaeb27230b3cdb36e7cfbd3
Evan Duncan <evan.duncan@target.com>
2015-02-11 16:12:21 -0600

    working reddit challenge

--- dailyprogrammer/challenge_201_easy.d
+++ dailyprogrammer/challenge_201_easy.d
@@ -25,10 +25,39 @@ import std.stdio, std.string, std.conv, std.datetime;
 
 void main()
 {
-    string[] challengeInputs = ["2015 7 4", "2015 10 31", "2015 12 24", "2016 1 1 ", "2016 2 9", "2020 1 1", "2020 2 9", "2020 3 1", "3015 2 9"];
-
     auto sysTime = split(Clock.currTime().toISOExtString, "-");
-    string currDate = format("%s %s %s ", sysTime[0], sysTime[1], sysTime[2][0 .. 2]);
+    int[] currentDate = parseDate(format("%s %s %s ", sysTime[0], sysTime[1], sysTime[2][0 .. 2]));
+
+    writeln("Enter a date (YYYY MM DD): ");
+    int[] inputDate = parseDate(readln());
+
+    printResults(countDays(currentDate, inputDate), currentDate, inputDate);
+}
+
+unittest
+{
+    assert(countDays([2015, 1 ,1], [2015, 12, 31]) == 364);
+    assert(countDays([2015, 1, 1], [2016, 3, 1]) == 425);
+}
+
+int countDays(int[] currentDate, int[] inputDate)
+{
+    int count;
+    while(currentDate[0] < inputDate[0]) {
+        count += isLeapYear(currentDate[0]) ? 366 : 365;
+        ++currentDate[0];
+    }
+
+    for(int i = 1; i < inputDate[1]; i++){
+        count += daysInMonth(i, inputDate[0]);
+    }
+
+    for(int i = 1; i < currentDate[1]; i++){
+        count -= daysInMonth(i, currentDate[1]);
+    }
+    count += (inputDate[2] - currentDate[2]);
+
+    return count;
 }
 
 unittest
@@ -47,13 +76,38 @@ bool isLeapYear(int year)
     return leapYear;
 }
 
-int daysUntil(string currentDate, string inputDate)
+unittest
 {
-    return 1;
-}  
+    assert(parseDate("2015 10 30") == [2015, 10, 30]);
+}
+
+int[] parseDate(string date)
+{
+    auto split_date = split(date);
+    int[] arr;
+    foreach(num; split_date) {
+        arr ~= to!int(num);
+    }
+    return arr;
+}
+
+unittest
+{
+    assert(daysInMonth(1, 2015) == 31);
+    assert(daysInMonth(2, 2000) == 29);
+    assert(daysInMonth(2, 2015) == 28);
+    assert(daysInMonth(11, 2015) == 30);
+}
 
 int daysInMonth(int month, int year)
 {
-    int[] days = [1: 31, 2: isLeapYear(year) ? 29 : 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31];
-    return days[month];
+    int[int] daysInMonth = [1: 31, 2: isLeapYear(year) ? 29 : 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31];
+    int days = daysInMonth[month];
+    return days;
+}
+
+void printResults(int count, int[] currentDate,  int[] inputDate)
+{
+    writefln("%u days from %u %u %u to %u %u %u", count, currentDate[0], currentDate[1], currentDate[2],
+        inputDate[0], inputDate[1], inputDate[2]);
 }
